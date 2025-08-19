06db7e8c27796ff915cef770b8218504649dae96
royysoans <roystonsoans3@gmail.com>
2025-08-19 23:34:04 +0530

    Final Changes

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ document.addEventListener('DOMContentLoaded',()=>{
   const textElement = document.querySelector('#intro-text');
   const originalText = textElement.textContent;
     textElement.textContent = '';
-
     let i = 0;
     const typingInterval=setInterval(()=>{
       if (i < originalText.length)
         {
-          textElement.textContent += originalText.charAt(i);
+          textElement.textContent+=originalText.charAt(i);
           i++;
         }
     },35);
